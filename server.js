485767a4e5bec001b7929491b24c805ac0d3738f
wangzongru <wang.zong@husky.neu.edu>
2020-04-09 14:00:42 -0400

    ss

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/web-angular'));
+app.use(express.static(__dirname + '/dist/whiteboard'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/web-angular/app.component.html'));});
-
+    '/dist/whiteboard/index.html'));});
 app.listen(process.env.PORT || 8080);
