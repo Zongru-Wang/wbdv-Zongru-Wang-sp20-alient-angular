e1f20a9dbfe467a4abecdd0934b09ffe38357335
wangzongru <wang.zong@husky.neu.edu>
2020-04-09 14:05:32 -0400

    ss

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/whiteboard'),
+      dir: require('path').join(__dirname, './coverage/wbdv-online-sp20-jannunzi-client-angular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
